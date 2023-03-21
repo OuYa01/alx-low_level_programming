d0dbde1bde28c7a29bd56b94d5390c42e76057d2
oussama laamri <laamri2002@gmail.com>
2023-03-21 23:35:35 +0000

    test

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,43 +1,23 @@
 #include "main.h"
-
 /**
-   * times_table - print out the 9 times table
-    *
-     * Return: void
-      */
-void times_table(void)
+ * jack_bauer - main
+ * Return: rien
+ */
+
+void jack_bauer(void)
 {
-	int v, h;
+	int h, m;
 
-	for (h = 0 ; h < 10 ; h++)
+	for (h = 0; h < 24; h++)
 	{
-		_putchar('0');
-		for (v = 1 ; v < 10 ; v++)
+		for (m = 0; m < 60; m++)
 		{
-			int sum = v * h;
-
-			_putchar(',');
-			if (sum / 100 == 0)
-			{
-				_putchar(32);
-			}
-			if (sum / 10 == 0)
-			{
-				_putchar(32);
-			}
-			else
-			{
-				_putchar('0' + (sum / 10));
-			}
-			if (sum % 10 == 0)
-			{
-				_putchar('0');
-			}
-			else
-			{
-				_putchar('0' + (sum % 10));
-			}
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
