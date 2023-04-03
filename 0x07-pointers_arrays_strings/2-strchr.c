a647326ae9a252b57801bca2126198eecc879250
oussama laamri <laamri2002@gmail.com>
2023-04-03 18:53:57 +0000

    hot fix

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,14 +10,17 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (; *s != '\0'; s++)
 	{
-		if (s[i] == c)
+		if (*s == c)
 		{
-			return (s + i);
+			return (s);
 		}
 	}
+	if (c == '\0')
+	{
+		return s;
+  	}
 	return (NULL);
 }
