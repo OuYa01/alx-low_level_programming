1669a1ca95a6ab8a216eaeca0e06ac5856a00576
oussama laamri <laamri2002@gmail.com>
2023-04-18 02:50:23 +0000

    just i fix a er

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,14 +17,17 @@ void print_dog(struct dog *d)
 
 	if (d->name == NULL)
 	{
-		d->name = "(nil)";
+		printf("Name: (nil)\n");
 	}
 	if (d->owner == NULL)
 	{
-		d->owner = "(nil)";
+		printf("Owner: (nil)\n");
 	}
 
-	printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
-	printf("Öwner: %s\n", d->owner);
+	else
+	{
+		printf("Name: %s\n", d->name);
+		printf("Age: %f\n", d->age);
+		printf("Öwner: %s\n", d->owner);
+	}
 }
