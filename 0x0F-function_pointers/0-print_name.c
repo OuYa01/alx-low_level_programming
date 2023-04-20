4383c9037852910c1050591cd6ff1191b70c995f
oussama laamri <laamri2002@gmail.com>
2023-04-20 02:01:19 +0000

    function that prints a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stddef.h>
+
+/**
+ * print_name - prints name
+ *
+ * @name: name thta will be print
+ * @f: pointer to a fnct
+ *
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL || f != NULL)
+		f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+# this folder is about Function pointers in C
