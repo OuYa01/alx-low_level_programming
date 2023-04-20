82f929b777a82517f87e71806e4269e96c3183e5
oussama laamri <laamri2002@gmail.com>
2023-04-20 02:52:00 +0000

    To hell with circumstances; I create opportunities

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+#include <stddef.h>
+
+/**
+ * int_index - function that searches for an integer
+ *
+ * @array: pointer of array
+ * @size: size of array
+ * @cmp: pointer of fnct
+ *
+ * Return: if scc 'how much integers?' else ret '-1'
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int integer;
+
+	if (array == NULL || cmp == NULL || size <= 0)
+	{
+		return (-1);
+	}
+
+	for (integer = 0; integer < size; integer++)
+	{
+		if (cmp(array[integer] != 0))
+		{
+			return (integer);
+		}
+	}
+
+	return (-1);
+}
