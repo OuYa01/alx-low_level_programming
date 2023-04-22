49bc3ccc3141fcf76b3656411936f5f010b0a3da
oussama laamri <laamri2002@gmail.com>
2023-04-22 01:53:06 +0000

    i add function that prints numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,33 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_numbers -  function that prints numbers
+ *
+ * @separator: pointers of comma
+ * @n: the number of number this fnc will print
+ *
+ * Return: nothing
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+
+	unsigned int p;
+	int print;
+
+	va_start(args, n);
+
+	for (p = 0; p < n; p++)
+	{
+		print = va_arg(args, int);
+		printf("%d", print);
+		if (print != 402)
+			printf("%s", separator);
+	}
+	printf("\n");
+
+
+	va_end(args);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,5 +2,7 @@
 #define VARIAIC_FUNCTIONS_H
 
 int sum_them_all(const unsigned int n, ...);
+int _putchar(char c);
+void print_numbers(const char *separator, const unsigned int n, ...);
 
 #endif
