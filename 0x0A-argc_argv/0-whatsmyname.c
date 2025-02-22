972d92b54f26f26910a4aa8247906108c1284950
OuYa1 <laamri2002@gmail.com>
2025-02-23 00:23:39 +0100

    i just fix a warning bcs a parameter in main is unuesd

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,7 +9,7 @@
  *
  * Return: Always 0 if succ
  */
-int main(int argc, char **argv)
+int main(int argc __attribute__((unused)), char **argv)
 {
 	printf("%s\n", argv[0]);
 
--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,7 +9,7 @@
  *
  * Return: Always 0 if succ
  */
-int main(int argc, char **argv)
+int main(int argc, char **argv __attribute__((unused)))
 {
 	printf("%d\n", --argc);
 
