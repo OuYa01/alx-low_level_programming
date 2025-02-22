edd8795e49b0ae823bb92b93eec4c11bcd2ed2b3
OuYa1 <laamri2002@gmail.com>
2025-02-23 00:14:37 +0100

    program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - print the name of programme
+ *
+ * @argc: is an integer variable that represents
+ * the number of arguments passed to the program
+ * @argv: is an array
+ *
+ * Return: Always 0 if succ
+ */
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
